5c3a3f404f6e861b80ffc78c81f1aa5ea04e5055
shivasinghal2000 <singhalshiva936@gmail.com>
2020-06-29 05:49:40 +0530

    Now reading user information from some another website componentDidMount() {

--- src/App.js
+++ src/App.js
@@ -8,10 +8,15 @@ class App extends React.Component {
   constructor () {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
   }
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => { this.setState({ robots: users }) });
+    }
 
   onSearchChange = (event) => {
     // console.log(event.target.value)
